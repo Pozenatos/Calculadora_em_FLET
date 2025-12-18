f8a21083fa5fffb22d3310fa3e994949ecac8fbb
Pozenatos <luishenrique.n.c.pozenato@gmail.com>
2025-12-17 22:20:34 -0300

    Commit pré adição de funções em relação aos operadores

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
 import flet as ft
+import math as mat
 
 def main(page: ft.Page):
 
@@ -18,25 +19,150 @@ def main(page: ft.Page):
             label_large=ft.TextStyle(size=20, weight=ft.FontWeight.BOLD),
             
             # title_medium é usado frequentemente em listas e subtítulos
-            title_medium=ft.TextStyle(size=26)
+            title_medium=ft.TextStyle(size=12)
         )
     )
     
-    result = ft.Text(
-        int(),
-        size=60
+    texto_visor = ft.Text(
+        str("0"),
+        size=60,
     )
 
-        
+    def limpar_tudo(e):
+        texto_visor.value = "0"
+        texto_visor.update()
+    
+    def digito0(e):
+        if len (texto_visor.value) == 12:
+            return
+
+        valor_atual = str(texto_visor.value)
+        if valor_atual == "0":
+            texto_visor.value = "0"
+        else:
+            texto_visor.value = valor_atual + "0" 
+        texto_visor.update()
+
+    def digito1(e):
+        if len (texto_visor.value) == 12:
+            return
+
+        valor_atual = str(texto_visor.value)
+        if valor_atual == "0":
+            texto_visor.value = "1"
+        else:
+            texto_visor.value = valor_atual + "1" 
+        texto_visor.update()
+
+    def digito2(e):
+        if len (texto_visor.value) == 12:
+            return
+
+        valor_atual = str(texto_visor.value)
+        if valor_atual == "0":
+            texto_visor.value = "2"
+        else:
+            texto_visor.value = valor_atual + "2" 
+        texto_visor.update()
+    
+    def digito3(e):
+        if len (texto_visor.value) == 12:
+            return
+
+        valor_atual = str(texto_visor.value)
+        if valor_atual == "0":
+            texto_visor.value = "3"
+        else:
+            texto_visor.value = valor_atual + "3" 
+        texto_visor.update()
+    
+    def digito4(e):
+        if len (texto_visor.value) == 12:
+            return
+
+        valor_atual = str(texto_visor.value)
+        if valor_atual == "0":
+            texto_visor.value = "4"
+        else:
+            texto_visor.value = valor_atual + "4" 
+        texto_visor.update()
+    
+    def digito5(e):
+        if len (texto_visor.value) == 12:
+            return
+
+        valor_atual = str(texto_visor.value)
+        if valor_atual == "0":
+            texto_visor.value = "5"
+        else:
+            texto_visor.value = valor_atual + "5" 
+        texto_visor.update()
+
     def digito6(e):
-        result.value = "6"  # Coloca o texto "6" (use aspas para ser texto)
-        result.update()     # <--- O SEGREDO! Manda o Flet redesenhar o texto na tela
-        print("Cliquei no 6") # Adicione isso para ver no terminal se funcionou
+        if len (texto_visor.value) == 12:
+            return
 
+        valor_atual = str(texto_visor.value)
+        if valor_atual == "0":
+            texto_visor.value = "6"
+        else:
+            texto_visor.value = valor_atual + "6" 
+        texto_visor.update()
+
+    def digito7(e):
+        if len (texto_visor.value) == 12:
+            return
+
+        valor_atual = str(texto_visor.value)
+        if valor_atual == "0":
+            texto_visor.value = "7"
+        else:
+            texto_visor.value = valor_atual + "7" 
+        texto_visor.update()
+
+    def digito8(e):
+        if len (texto_visor.value) == 12:
+            return
+
+        valor_atual = str(texto_visor.value)
+        if valor_atual == "0":
+            texto_visor.value = "8"
+        else:
+            texto_visor.value = valor_atual + "8" 
+        texto_visor.update()
+
+    def digito9(e):
+        if len (texto_visor.value) == 12:
+            return
+
+        valor_atual = str(texto_visor.value)
+        if valor_atual == "0":
+            texto_visor.value = "9"
+        else:
+            texto_visor.value = valor_atual + "9" 
+        texto_visor.update() 
+    
+    def apagar(e):
+        valor_atual = str(texto_visor.value)
+
+        if len(valor_atual) > 1:
+            texto_visor.value = valor_atual[:-1]
+        
+        else:
+            texto_visor.value = "0"
+        texto_visor.update()
+
+    def digito_virgula(e):
+        valor_atual = str(texto_visor.value)
+        if valor_atual == "0":
+            texto_visor.value = "0"
+
+        else:
+            texto_visor.value = valor_atual + "," 
+        texto_visor.update()  
   
-
     visor = ft.Container( 
-        result,
+        texto_visor,
         bgcolor="black",
         width=float("inf"),
         expand=2,
@@ -56,6 +182,8 @@ def main(page: ft.Page):
                                 alignment=ft.alignment.center,
                                 ink=True,
                                 expand=1,
+
+                                on_click = limpar_tudo
                                 
                             ),
                             ft.Container( #Segunda Linha
@@ -64,6 +192,8 @@ def main(page: ft.Page):
                                     alignment=ft.alignment.center,
                                     ink=True,    
                                     expand=1,
+
+                                     on_click = digito7
                             ),
                             ft.Container( #Terceira Linha
                                     ft.Text("4", color="white", size=30 ),
@@ -71,6 +201,8 @@ def main(page: ft.Page):
                                     alignment=ft.alignment.center,
                                     ink=True,    
                                     expand=1,
+
+                                    on_click = digito4
                             ),
                             ft.Container( #Quarta Linha
                                     ft.Text("1", color="white", size=30 ),
@@ -78,6 +210,8 @@ def main(page: ft.Page):
                                     alignment=ft.alignment.center,
                                     ink=True,     
                                     expand=1,
+
+                                    on_click = digito1
                             ),
                             ft.Container( #Quinta Linha
                                     ft.Text("⌫", color="#FF8400", size=30 ),
@@ -85,6 +219,8 @@ def main(page: ft.Page):
                                     alignment=ft.alignment.center,
                                     ink=True,    
                                     expand=1,
+
+                                    on_click = apagar
                             ),
 
                         ]
@@ -107,6 +243,8 @@ def main(page: ft.Page):
                                     alignment=ft.alignment.center,
                                     ink=True,   
                                     expand=1,
+
+                                    on_click = digito8
                             ),
                             ft.Container( #Terceira Linha
                                     ft.Text("5", color="white", size=30 ),
@@ -114,6 +252,8 @@ def main(page: ft.Page):
                                     alignment=ft.alignment.center,
                                     ink=True,    
                                     expand=1,
+
+                                    on_click = digito5
                             ),
                             ft.Container( #Quarta Linha
                                     ft.Text("2", color="white", size=30 ),
@@ -121,6 +261,8 @@ def main(page: ft.Page):
                                     alignment=ft.alignment.center,
                                     ink=True,    
                                     expand=1,
+
+                                    on_click = digito2
                             ),
                             ft.Container( #Quinta Linha
                                     ft.Text("0", color="white", size=30 ),
@@ -128,6 +270,8 @@ def main(page: ft.Page):
                                     alignment=ft.alignment.center,
                                     ink=True,    
                                     expand=1,
+
+                                    on_click = digito0
                             ),
                             
 
@@ -151,6 +295,8 @@ def main(page: ft.Page):
                                     alignment=ft.alignment.center,
                                     ink=True,    
                                     expand=1,
+
+                                    on_click = digito9
                             ),
                             ft.Container( #Terceira Linha
                                     ft.Text("6", color="white", size=30 ),
@@ -158,6 +304,7 @@ def main(page: ft.Page):
                                     alignment=ft.alignment.center,
                                     ink=True,   
                                     expand=1,
+
                                     on_click = digito6  
                             ),
                             ft.Container( #Quarta Linha
@@ -166,6 +313,8 @@ def main(page: ft.Page):
                                     alignment=ft.alignment.center,
                                     ink=True,   
                                     expand=1,
+
+                                    on_click = digito3
                             ),
                             ft.Container( #Quinta Linha
                                     ft.Text(",", color="white", size=30 ),
@@ -173,6 +322,8 @@ def main(page: ft.Page):
                                     alignment=ft.alignment.center,
                                     ink=True,    
                                     expand=1,
+
+                                    on_click = digito_virgula
                             ),
 
                         ]
